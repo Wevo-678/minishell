1af1f06aacf95fffa5fb7aeb05805226b32d6d8f
Pierre Carlier <picarlie@c2r7p2.42perpignan.fr>
2024-10-25 18:08:13 +0200

    debut split

--- src/parsing/split_them_all.c
+++ src/parsing/split_them_all.c
@@ -1,13 +1,41 @@
 #include "../../includes/minishell.h"
 
+void	*split_init2(t_node **node)
+{
+	int		i;
+	int		len;
+	int		new_word;
 
+	i = 0;
+	new_word = 1;
+	len = cmd_len((*node)->data_dup, i);
+	while ((*node)->data_dup[i])
+		{
+			if (new_word)
+			{
+				(*node)->data
+				new_word = 0;
+			}
+			if (len == -2) //a remplir
+				i++;
+			i++;
+		}
+}
 
-// int main(int argc, char **argv)
-// {
-// 	printf("str : %s\n", argv[1]);
-// 	printf("len : %d\n", cmd_len(argv[1], 0));
-// 	return (0);
-// }
+void	*split_init(t_node **first)
+{
+	t_node	*tmp;
+
+	tmp = (*first);
+	while (tmp->next)
+	{
+		tmp->data = (t_node**)malloc(sizeof(t_node*) * (count_cmd(tmp->data_dup)));
+		if (!(tmp->data))
+			return (NULL);
+		split_init2(&tmp);
+		tmp = tmp->next;
+	}
+}
 
 /* /!\/!\/!\/!\/!\ A faire echo"test" a remplacer par echotest
 ie : enlever les quotes  et remplacer les variables si necessaire
