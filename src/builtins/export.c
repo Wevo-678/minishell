70f391be24ed4c920964bfd67e125e7d9f48fcff
Pierre Carlier <picarlie@c2r4p6.42perpignan.fr>
2024-11-04 16:30:44 +0100

    norme

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,42 +1,52 @@
 #include "../../includes/minishell.h"
 
-int update_env_var(char *name, char *value, char ***envp, int index)
+int	update_env_var(char *name, char *value, char ***envp, int index)
 {
-    char *new_entry = malloc(strlen(name) + strlen(value) + 2);
-    if (!new_entry)
-        return (1);
-    sprintf(new_entry, "%s=%s", name, value);
-    free((*envp)[index]);
-    (*envp)[index] = new_entry;
-    return (0);
+	char	*new_entry;
+
+	new_entry = malloc(strlen(name) + strlen(value) + 2);
+	if (!new_entry)
+		return (1);
+	sprintf(new_entry, "%s=%s", name, value);
+	free((*envp)[index]);
+	(*envp)[index] = new_entry;
+	return (0);
 }
 
-int add_env_var(char *name, char *value, char ***envp, int i)
+int	add_env_var(char *name, char *value, char ***envp, int i)
 {
-    char *new_entry = malloc(strlen(name) + strlen(value) + 2);
-    if (!new_entry)
-        return (1);
-    sprintf(new_entry, "%s=%s", name, value);
-    *envp = ft_realloc((void **)(*envp), sizeof(char *) * (i + 1), sizeof(char *) * (i + 2));
-    if (!*envp)
-        return (1);
-    (*envp)[i] = new_entry;
-    (*envp)[i + 1] = NULL;
-    return (0);
+	char	*new_entry;
+
+	new_entry = malloc(strlen(name) + strlen(value) + 2);
+	if (!new_entry)
+		return (1);
+	sprintf(new_entry, "%s=%s", name, value);
+	*envp = ft_realloc((void **)(*envp), sizeof(char *) * (i + 1),
+			sizeof(char *) * (i + 2));
+	if (!*envp)
+		return (1);
+	(*envp)[i] = new_entry;
+	(*envp)[i + 1] = NULL;
+	return (0);
 }
 
-int ft_export(char **args, char ***envp)
+int	ft_export(char **args, char ***envp)
 {
-    int i = 0;
-    char *name = ft_strtok(args[1], "=");
-    char *value = ft_strtok(NULL, "=");
-    if (!name || !value)
-        return (1);
-    while ((*envp)[i])
-    {
-        if (strncmp((*envp)[i], name, strlen(name)) == 0 && (*envp)[i][strlen(name)] == '=')
-            return update_env_var(name, value, envp, i);
-        i++;
-    }
-    return add_env_var(name, value, envp, i);
+	int		i;
+	char	*name;
+	char	*value;
+
+	i = 0;
+	name = ft_strtok(args[1], "=");
+	value = ft_strtok(NULL, "=");
+	if (!name || !value)
+		return (1);
+	while ((*envp)[i])
+	{
+		if (strncmp((*envp)[i], name, strlen(name)) == 0 &&
+				(*envp)[i][strlen(name)] == '=')
+			return (update_env_var(name, value, envp, i));
+		i++;
+	}
+	return (add_env_var(name, value, envp, i));
 }
--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -13,41 +13,35 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-
-
-
-// ft_strtok fonctionne comme strtok, en coupant les tokens en fonction des délimiteurs
+// ft_strtok fonctionne comme strtok,
+// en coupant les tokens en fonction des délimiteurs
 char	*ft_strtok(char *str, const char *delim)
 {
-	static char *save_ptr;
+	static char	*save_ptr;
 	char		*start;
 	char		*end;
 
 	// Si `str` n'est pas NULL, réinitialiser `save_ptr`
 	if (str)
 		save_ptr = str;
-
-	// Si `save_ptr` est NULL ou la fin de la chaîne est atteinte, il n'y a plus rien à parser
+	// Si `save_ptr` est NULL ou la fin de la chaîne est atteinte,
+	//il n'y a plus rien à parser
 	if (!save_ptr || *save_ptr == '\0')
 		return (NULL);
-
 	// Avancer jusqu'à trouver un caractère qui n'est pas un délimiteur
 	start = save_ptr;
 	while (*start && ft_strchr(delim, *start))
 		start++;
-
 	// Si on atteint la fin de la chaîne après les délimiteurs, retour NULL
 	if (*start == '\0')
 	{
 		save_ptr = NULL;
 		return (NULL);
 	}
-
 	// Trouver la fin du token
 	end = start;
 	while (*end && !ft_strchr(delim, *end))
 		end++;
-
 	// Terminer le token avec un '\0' si ce n'est pas la fin de la chaîne
 	if (*end != '\0')
 	{
@@ -56,13 +50,13 @@ char	*ft_strtok(char *str, const char *delim)
 	}
 	else
 		save_ptr = NULL;
-
 	return (start);
 }
 
 int	ft_unset(char **args, char ***envp)
 {
-	int		i, j;
+	int		i;
+	int		j;
 	size_t	len;
 
 	if (!args[1])
