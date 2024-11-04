4675cbf9e4db2f6dc689a5dffe5d7cfa3ed84605
Pierre Carlier <picarlie@c2r4p6.42perpignan.fr>
2024-11-04 18:25:26 +0100

    added free for the t_list and its content

--- src/utils/ft_lstclear.c
+++ src/utils/ft_lstclear.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:27:07 by alribeyr          #+#    #+#             */
-/*   Updated: 2024/10/30 12:20:58 by alex             ###   ########.fr       */
+/*   Updated: 2024/11/04 18:24:43 by picarlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/libft.h"
+#include "../../includes/minishell.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
@@ -24,4 +24,26 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		*lst = temp;
 	}
 	lst = NULL;
+}
+
+/* Clear the t_node list and its content */
+void	clear_list(t_node **first)
+{
+	t_node	*tmp;
+	int		i;
+
+	while (tmp)
+	{
+		tmp = (*first)->next;
+		i = 0;
+		while (tmp->data[i])
+		{
+			free(tmp->data[i]);
+			i++;
+		}
+		free(tmp->data);
+		free(*first);
+		(*first) = tmp;
+	}
+	first = (NULL);
 }
