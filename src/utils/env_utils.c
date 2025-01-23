4da8509ae215e240885b198bfdda5c006e42200b
Maxime Benet <mabenet@c3r1p2.42perpignan.fr>
2025-01-23 16:35:22 +0100

    patch cd

--- src/utils/env_utils.c
+++ src/utils/env_utils.c
@@ -18,44 +18,65 @@ char	*get_env_value(char **envp, const char *name)
 	return ("Error dir no find");
 }
 
-int	set_env_value(char ***envp, const char *name, const char *value)
-{
-	int		len;
-	int		count;
-	char	**new_env;
-	char	*new_entry;
-	int		i;
+static char *create_env_entry(const char *name, const char *value) {
+    char *new_entry;
+    if (!name || !value)
+        return NULL;
 
-	len = ft_strlen(name);
-	i = 0;
-	new_entry = malloc(ft_strlen(name) + ft_strlen(value) + 2);
-	if (!new_entry)
-		return (-1);
-	ft_strcpy(new_entry, name);
-	ft_strcat(new_entry, "=");
-	ft_strcat(new_entry, value);
-	while ((*envp)[i])
+    new_entry = malloc(strlen(name) + strlen(value) + 2);
+    if (!new_entry)
+        return NULL;
+
+    strcpy(new_entry, name);
+    strcat(new_entry, "=");
+    strcat(new_entry, value);
+    return new_entry;
+}
+
+static int update_env_value(char **envp, const char *name, char *new_entry)
+{
+    int len = strlen(name);
+    for (int i = 0; envp[i]; i++) 
 	{
-		if (ft_strncmp((*envp)[i], name, len) == 0 && (*envp)[i][len] == '=')
+        if (strncmp(envp[i], name, len) == 0 && envp[i][len] == '=') 
 		{
-			free((*envp)[i]);
-			(*envp)[i] = new_entry;
-			return (0);
-		}
-		i++;
+            free(envp[i]);
+            envp[i] = new_entry;
+            return 1; 
+    	}
 	}
-	count = ft_arraylen(*envp);
-	new_env = (char **)ft_realloc((void **)*envp, count * sizeof(char *),
-			(count + 2) * sizeof(char *));
-	if (!new_env)
-	{
-		free(new_entry);
-		return (-1);
-	}
-	free(new_env[count]);
-	new_env[count] = new_entry;
-	new_env[count + 1] = NULL;
-	free(*envp);
-	*envp = new_env;
-	return (0);
+    return 0;
+}
+
+
+static int add_env_entry(char ***envp, char *new_entry) {
+    int count = 0;
+    while ((*envp)[count])
+        count++;
+
+    char **new_env = realloc(*envp, (count + 2) * sizeof(char *));
+    if (!new_env) {
+        free(new_entry);
+        return -1;
+    }
+
+    new_env[count] = new_entry;
+    new_env[count + 1] = NULL;
+    *envp = new_env;
+    return 0;
+}
+
+int set_env_value(char ***envp, const char *name, const char *value) 
+{
+    if (!envp || !name || !value)
+        return -1;
+
+    char *new_entry = create_env_entry(name, value);
+    if (!new_entry)
+        return -1;
+
+    if (update_env_value(*envp, name, new_entry))
+        return 0;
+
+    return (add_env_entry(envp, new_entry));
 }
