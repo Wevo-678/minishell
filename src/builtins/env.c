cc2d0a99d0876e882fb08a4b93c91e75baa1b96d
Pierre Carlier <picarlie@c1r2p1.42perpignan.fr>
2025-01-28 17:58:33 +0100

    env.c ok

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:05:50 by picarlie          #+#    #+#             */
-/*   Updated: 2025/01/27 14:05:51 by picarlie         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:57:53 by picarlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,16 @@ int	ft_env(char **envp)
 		printf("%s\n", envp[i]);
 		i++;
 	}
+	return (0);
+}
+
+int	ft_shlvl(char **new_shlvl, char **level_str, char ****envp, int i)
+{
+	ft_strcpy(*new_shlvl, "SHLVL=");
+	ft_strcat(*new_shlvl, *level_str);
+	free(*level_str);
+	free((**envp)[i]);
+	(**envp)[i] = *new_shlvl;
 	return (0);
 }
 
@@ -41,17 +51,8 @@ int	ft_increment_shlvl(char ***envp)
 			shlvl = &((*envp)[i][6]);
 			level = ft_atoi(shlvl) + 1;
 			level_str = ft_itoa(level);
-			if (!level_str)
-				return (1);
 			new_shlvl = malloc(ft_strlen("SHLVL=") + ft_strlen(level_str) + 1);
-			if (!new_shlvl)
-				return (1);
-			ft_strcpy(new_shlvl, "SHLVL=");
-			ft_strcat(new_shlvl, level_str);
-			free(level_str);
-			free((*envp)[i]);
-			(*envp)[i] = new_shlvl;
-			return (0);
+			return (ft_shlvl(&new_shlvl, &level_str, &envp, i));
 		}
 		i++;
 	}
